993bdc4698d57f2ffbc2debe69b1ec3df994ed43
Brad Smith <brad@smithwebtek.com>
2019-11-12 09:22:35 -0500

    Schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_150759) do
+ActiveRecord::Schema.define(version: 2019_11_08_203238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2019_11_08_150759) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "title"
+    t.integer "resource_id"
+    t.string "url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "resource_tags", force: :cascade do |t|
